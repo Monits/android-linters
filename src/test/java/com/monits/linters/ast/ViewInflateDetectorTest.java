feb3bbe227ba83ab08ca30699eab00f57b6d5b97
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-05-23 14:55:34 -0300

    Improvements to ViewInflateDetector
     - We can now detect static imports of the method
     - We can now detects calls through the hierarchy, either static or through
        inheritance
     - We no longer have false posiives when anothe class named View is used
     - But we now need to be able to compile through the whole class hierarchy...

--- src/test/java/com/monits/linters/ast/ViewInflateDetectorTest.java
+++ src/test/java/com/monits/linters/ast/ViewInflateDetectorTest.java
@@ -28,7 +28,6 @@ import com.google.common.base.Joiner;
 import com.monits.linters.test.AbstractTestCase;
 import com.monits.linters.test.matchers.WarningMatcherBuilder;
 
-
 public class ViewInflateDetectorTest extends AbstractTestCase {
 
 	@Override
@@ -50,6 +49,7 @@ public class ViewInflateDetectorTest extends AbstractTestCase {
 
 	public void testViewInflateIsReported() throws Exception {
 		lintProject(
+			androidView(),
 			java("src/ViewInflate.java",
 				Joiner.on('\n').join(
 						"import android.view.View;",
@@ -69,8 +69,53 @@ public class ViewInflateDetectorTest extends AbstractTestCase {
 				.build()));
 	}
 	
+	public void testViewInflateSamePackageIsReported() throws Exception {
+		lintProject(
+			androidView(),
+			java("src/android/view/ViewInflate.java",
+				Joiner.on('\n').join(
+						"package android.view;",
+						"public class ViewInflate {",
+						"	public void myMethod() {",
+						"		View.inflate(null, 1, null);",
+						"	}",
+						"}"
+					)
+				));
+
+		assertThat("Failed to detect call to View.inflate", getWarnings(),
+			Matchers.hasItem(new WarningMatcherBuilder()
+				.fileName("ViewInflate.java")
+				.line(4)
+				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
+				.build()));
+	}
+	
+	public void testViewInflateStarImportIsReported() throws Exception {
+		lintProject(
+			androidView(),
+			java("src/ViewInflate.java",
+				Joiner.on('\n').join(
+						"import android.view.*;",
+						"public class ViewInflate {",
+						"	public void myMethod() {",
+						"		View.inflate(null, 1, null);",
+						"	}",
+						"}"
+					)
+				));
+
+		assertThat("Failed to detect call to View.inflate using star import", getWarnings(),
+			Matchers.hasItem(new WarningMatcherBuilder()
+				.fileName("ViewInflate.java")
+				.line(4)
+				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
+				.build()));
+	}
+	
 	public void testViewInflateFQCNIsReported() throws Exception {
 		lintProject(
+			androidView(),
 			java("src/ViewInflateFQCN.java",
 				Joiner.on('\n').join(
 						"public class ViewInflateFQCN {",
@@ -85,6 +130,75 @@ public class ViewInflateDetectorTest extends AbstractTestCase {
 			Matchers.hasItem(new WarningMatcherBuilder()
 				.fileName("ViewInflateFQCN.java")
 				.line(3)
+				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
+				.build()));
+	}
+
+	public void testViewInflateStaticImportIsReported() throws Exception {
+		lintProject(
+			java("src/ViewInflateStaticImport.java",
+				Joiner.on('\n').join(
+						"import static android.view.View.inflate;",
+						"public class ViewInflateStaticImport {",
+						"	public void myMethod() {",
+						"		inflate(null, 1, null);",
+						"	}",
+						"}"
+					)
+				));
+
+		assertThat("Failed to detect call to View.inflate using static import", getWarnings(),
+			Matchers.hasItem(new WarningMatcherBuilder()
+				.fileName("ViewInflateStaticImport.java")
+				.line(4)
+				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
+				.build()));
+	}
+	
+	public void testViewInflateIsReportedWithoutOperand() throws Exception {
+		lintProject(
+			androidLinearLayout(),
+			androidViewGroup(),
+			androidView(),
+			java("src/ViewInflate.java",
+				Joiner.on('\n').join(
+						"import android.widget.LinearLayout;",
+						"public class ViewInflate extends LinearLayout {",
+						"	public void myMethod() {",
+						"		inflate(null, 1, null);",
+						"	}",
+						"}"
+					)
+				));
+
+		assertThat("Failed to detect call to View.inflate", getWarnings(),
+			Matchers.hasItem(new WarningMatcherBuilder()
+				.fileName("ViewInflate.java")
+				.line(4)
+				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
+				.build()));
+	}
+	
+	public void testLinearLayoutInflateIsReported() throws Exception {
+		lintProject(
+			androidLinearLayout(),
+			androidViewGroup(),
+			androidView(),
+			java("src/ViewInflate.java",
+				Joiner.on('\n').join(
+						"import android.widget.LinearLayout;",
+						"public class ViewInflate {",
+						"	public void myMethod() {",
+						"		LinearLayout.inflate(null, 1, null);",
+						"	}",
+						"}"
+					)
+				));
+
+		assertThat("Failed to detect call to LinearLayout.inflate", getWarnings(),
+			Matchers.hasItem(new WarningMatcherBuilder()
+				.fileName("ViewInflate.java")
+				.line(4)
 				.message(ViewInflateDetector.VIEW_INFLATE_MSG)
 				.build()));
 	}
@@ -105,5 +219,37 @@ public class ViewInflateDetectorTest extends AbstractTestCase {
 
 		assertThat("View.inflate detected even when targeting lollpop", getWarnings(),
 			Matchers.empty());
+	}
+	
+	private TestFile androidView() {
+		return java("src/android/view/View.java",
+			Joiner.on('\n').join(
+					"package android.view;",
+					"public class View {",
+					"}"
+				)
+			);
+	}
+	
+	private TestFile androidLinearLayout() {
+		return java("src/android/widget/LinearLayout.java",
+			Joiner.on('\n').join(
+					"package android.widget;",
+					"import android.view.ViewGroup;",
+					"public class LinearLayout extends ViewGroup {",
+					"}"
+				)
+			);
+	}
+	
+	private TestFile androidViewGroup() {
+		return java("src/android/view/ViewGroup.java",
+			Joiner.on('\n').join(
+					"package android.view;",
+					"import android.view.View;",
+					"public class ViewGroup extends View {",
+					"}"
+				)
+			);
 	}
 }
